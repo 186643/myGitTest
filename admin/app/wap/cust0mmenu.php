2a0bada0371ba1e346f3be77a32c9802cfa3dd9d
186643 <postmaster@lystk1958.com.cn>
2017-04-21 07:38:27 +0800

    Create cust0mmenu.php

---
+++ admin/app/wap/cust0mmenu.php
@@ -0,0 +1,47 @@
+<? php
+# Lystk1958   Enterprise Content Management System 
+# Copyright (C) Lystk1958  Co.,Ltd (http://www.lystk1958.com.cn). All rights reserved. 
+$depth='../';
+require_once $depth.'../login/login_check.php';
+require_once $depth.'../include/pager.class.php';
+if($action){
+    $sign=explode(",",$allid);
+	if($action=='editor'){
+		foreach($sign as $key=>$val){
+			$vals='name_'.$val;
+			$valsmun=$$vals;
+			$valsmun = mb_convert_encoding($valsmun, "GBK","UTF-8");
+			$valsmun=strlen($valsmun);
+			$sequence='sequence_'.$val;
+			$sequences=$$sequence;
+			$name='name_'.$val;
+			$names=$$name;
+			if($valsmun>8){
+				metsave('../app/wap/custommenu.php?anyid='.$anyid.'&cs='.$cs.'&lang='.$lang,'名称最多支持4个汉字字符（英文字符算半个汉字字符）',$depth);
+			}
+			$query = "UPDATE $met_wapmenu SET sequence='$sequences', name='$names'  where id='$val'";
+			$menus=$db->query($query);
+		
+		}
+		metsave('../app/wap/custommenu.php?anyid='.$anyid.'&cs='.$cs.'&lang='.$lang,'操作成功',$depth);
+	}
+	if($action=='del'){
+		foreach($sign as $key=>$val){
+			$querys = "DELETE FROM $met_wapmenu where id='$val'";
+			$menus=$db->query($querys);	
+		}	
+		metsave('../app/wap/custommenu.php?anyid='.$anyid.'&cs='.$cs.'&lang='.$lang,'操作成功',$depth);
+	}
+
+}
+$query = "SELECT * FROM $met_wapmenu where lang='$lang' ORDER BY sequence asc";
+$result = $db->query($query);
+while($list = $db->fetch_array($result)) {
+	$list_array2[]=$list;	
+}
+$css_url=$depth."../templates/".$met_skin."/css";
+$img_url=$depth."../templates/".$met_skin."/images";
+include template('app/wap/custommenu');
+footer();
+# Copyright (C) Lystk1958 Co., Ltd. (http://www.lystk1958.com.cn). All rights reserved.
+?>

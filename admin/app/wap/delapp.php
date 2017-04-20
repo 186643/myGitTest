a82d335c8d3aeb20bb7756a45be74ee1f0e9fb23
186643 <postmaster@lystk1958.com.cn>
2017-04-21 07:40:03 +0800

    Create delapp.php

---
+++ admin/app/wap/delapp.php
@@ -0,0 +1,8 @@
+<? php
+# Lystk1958   Enterprise Content Management System 
+# Copyright (C) Lystk1958  Co.,Ltd (http://www.lystk1958.com.cn). All rights reserved. 
+$depth='../';
+require_once $depth.'../login/login_check.php';
+echo '禁止删除';
+#Copyright (C) Lystk1958 Co., Ltd. (http://www.lystk1958.com.cn). All rights reserved.
+?>
